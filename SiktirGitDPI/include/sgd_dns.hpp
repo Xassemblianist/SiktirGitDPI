95119e22745638b0e19f73f3459d5279438deba8
Xassemblianist <106768809+Xassemblianist@users.noreply.github.com>
2026-02-09 22:30:33 +0300

    Delete SiktirGitDPI directory

--- SiktirGitDPI/include/sgd_dns.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "sgd_core.hpp"
-
-namespace SGDPI {
-    namespace DNS {
-        bool IsDNSQuery(PacketContext& ctx);
-        void BlockDoH(PacketContext& ctx);
-    }
-}
--- SiktirGitDPI/include/sgd_http.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "sgd_core.hpp"
-
-namespace SGDPI {
-    namespace HTTP {
-        bool IsHttpRequest(const char* payload, UINT len);
-        bool FragmentHostHeader(char* payload, UINT len);
-    }
-}
